3fa614d6837d880a0d356bc384fb26d79cade789
KairavA <kairav.kadakia@gmail.com>
2022-02-19 13:06:00 +0530

    SuperSimpleStocks

---
+++ models.py
@@ -0,0 +1,95 @@
+import datetime
+import logging
+import numpy as np
+import abc
+from enum import Enum
+
+logger = logging.getLogger(__name__)
+
+
+class ACTION(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class SYMBOL(Enum):
+    TEA = "TEA"
+    POP = "POP"
+    ALE = "ALE"
+    GIN = "GIN"
+    JOE = "JOE"
+
+
+class Trade(object):
+
+    def __init__(self, stock, quantity, buy_sell_indicator, price):
+        self.Stock = stock
+        self.Quantity = quantity
+        self.BuySellIndicator = buy_sell_indicator
+        self.Price = price
+        self.TimeStamp = datetime.datetime.utcnow().timestamp()
+
+
+class Stock(abc.ABC):
+
+    def __init__(self, stock_symbol, last_divided, fixed_dividend, par_value):
+        self.Symbol = stock_symbol
+        self.LastDividend = last_divided
+        self.FixedDividend = fixed_dividend
+        self.ParValue = par_value
+
+    @abc.abstractmethod
+    def dividend_yield(self, price):
+        pass
+
+    def pe_ratio(self, price):
+        return price/self.LastDividend if self.LastDividend else 0
+
+
+class CommonStock(Stock):
+
+    def dividend_yield(self, price):
+        return self.LastDividend/price
+
+
+class PreferredStock(Stock):
+
+    def dividend_yield(self, price):
+        return (self.FixedDividend * self.ParValue) / price
+
+
+class GlobalBeverageCorporationExchange(object):
+
+    def __init__(self):
+        self._AllTrades = []
+
+    def record_trade(self, stock, quantity, action, price):
+        try:
+            trade = Trade(stock, quantity, action, price)
+            self._AllTrades.append(trade)
+            return trade
+        except Exception as ex:
+            logger.error(f"Error when recording the trade with values: {stock.Symbol}, {quantity}, {action}, {price}")
+            logger.error(ex)
+            raise ex
+
+    def get_recent_trades(self, symbol=None, time_gap_minutes=None):
+        last_time_stamp = (datetime.datetime.utcnow() - datetime.timedelta(minutes=time_gap_minutes)).timestamp() if \
+            time_gap_minutes else None
+        return [trade for trade in self._AllTrades if ((not last_time_stamp) or trade.TimeStamp >
+                last_time_stamp) and ((not symbol) or trade.Stock.Symbol == symbol)]
+
+    def get_all_share_index(self):
+        traded_stocks = set(transaction.Stock for transaction in self._AllTrades)
+        vw_all_stocks = np.array([self.volume_weighted_stock_price(symbol=stock.Symbol) for stock in traded_stocks])
+        all_share_index = vw_all_stocks.prod() ** (1.0 / len(traded_stocks))
+        logger.info(f"all_share_index:{all_share_index}")
+        return all_share_index
+
+    def volume_weighted_stock_price(self, symbol=None, time_gap_minutes=None):
+        recent_trades = self.get_recent_trades(symbol, time_gap_minutes)
+        total_value = sum((trade.Quantity*trade.Price) for trade in recent_trades)
+        total_quantity = sum(trade.Quantity for trade in recent_trades)
+        vwsp = total_value/total_quantity
+        logger.info(f"vwsp for {symbol}:{vwsp}")
+        return vwsp
---
+++ unittests.py
@@ -0,0 +1,40 @@
+import unittest
+from models import GlobalBeverageCorporationExchange, CommonStock, PreferredStock, Trade, ACTION, SYMBOL
+import random
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TestGlobalBeverageCorporationExchange(unittest.TestCase):
+
+    def test_save(self):
+        GBCE_object = GlobalBeverageCorporationExchange()
+        stock = CommonStock(SYMBOL.TEA, 0, 0, 100)
+        trade = GBCE_object.record_trade(stock, 100, ACTION.BUY, 11.0)
+        self.assertTrue(isinstance(trade, Trade))
+        saved_transaction = GBCE_object.get_recent_trades()[0]
+        self.assertEqual(trade.Stock.Symbol, saved_transaction.Stock.Symbol)
+
+    def test_all_share_index(self):
+        GBCE_object = GlobalBeverageCorporationExchange()
+        stocks = [CommonStock(SYMBOL.TEA, 0, 0, 100), CommonStock(SYMBOL.POP, 8, 0, 100), CommonStock(SYMBOL.ALE, 23, 0, 60),
+                  PreferredStock(SYMBOL.GIN, 8, 2, 100), CommonStock(SYMBOL.JOE, 13, 0, 250)]
+
+        for stock in stocks:
+            for i in range(1, 20):
+                trade = GBCE_object.record_trade(stock, random.randint(100, 1000), random.choice(list(ACTION)),
+                                                 random.random() * random.randint(100,500))
+                trade.TimeStamp = (datetime.datetime.utcnow() - datetime.timedelta(minutes=random.randint(0, 10)))\
+                    .timestamp()
+
+        all_share_index = GBCE_object.get_all_share_index()
+        self.assertIsNotNone(all_share_index)
+        for stock in stocks:
+            vwsp = GBCE_object.volume_weighted_stock_price(symbol=stock.Symbol, time_gap_minutes=5)
+            self.assertIsNotNone(vwsp)
+
+
+if __name__ == '__main__':
+    unittest.main()
